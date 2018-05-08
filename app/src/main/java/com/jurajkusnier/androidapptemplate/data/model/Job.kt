167ad8a07ab3b915d68468cd928325d52bb3446a
jurajkusnier <jurajkusnier@gmail.com>
2018-05-08 16:01:51 +0200

    Data class changes

--- app/src/main/java/com/jurajkusnier/androidapptemplate/data/model/Job.kt
+++ app/src/main/java/com/jurajkusnier/androidapptemplate/data/model/Job.kt
@@ -1,13 +1,13 @@
 package com.jurajkusnier.androidapptemplate.data.model
 
-data class Job (val id:String?,
-                val created_at:String?,
-                val title:String?,
-                val location:String?,
-                val type:String?,
-                val description:String?,
-                val how_to_apply:String?,
-                val company:String?,
+data class Job (val id:String,
+                val created_at:String,
+                val title:String,
+                val location:String,
+                val type:String,
+                val description:String,
+                val how_to_apply:String,
+                val company:String,
                 val company_url:String?,
                 val company_logo:String?,
-                val url:String?)
+                val url:String)
