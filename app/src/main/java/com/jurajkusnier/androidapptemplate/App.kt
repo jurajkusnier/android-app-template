ce045e4dcd7a359ebcc99db728b2581cf3c2b409
jurajkusnier <jurajkusnier@gmail.com>
2018-08-27 17:22:50 +0200

    LeakCanary implementation

--- app/src/main/java/com/jurajkusnier/androidapptemplate/App.kt
+++ app/src/main/java/com/jurajkusnier/androidapptemplate/App.kt
@@ -1,10 +1,30 @@
 package com.jurajkusnier.androidapptemplate
 
+import android.content.Context
+import android.support.multidex.MultiDex
 import com.jurajkusnier.androidapptemplate.di.DaggerAppComponent
+import com.squareup.leakcanary.LeakCanary
 import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 
 class App: DaggerApplication() {
 
     override fun applicationInjector(): AndroidInjector<out DaggerApplication> = DaggerAppComponent.builder().application(this).build()
+
+    override fun attachBaseContext(base: Context) {
+        super.attachBaseContext(base)
+        MultiDex.install(this)
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+        // Normal app init code..
+    }
 }
